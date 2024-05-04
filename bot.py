4b0f893e1dff033df7fcdd0acef2d543c4b3065f
SuperGops7 <gopikrishnank99@gmail.com>
2024-05-04 13:53:30 +0530

    Updated bot to return different quote types

--- bot.py
+++ bot.py
@@ -13,18 +13,27 @@ def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
 
 @bot.message_handler(commands=['quote'])
-def quote_handler(message):
-    quote, author, error = quotes_getter.get_my_quote(category='inspirational')
-    if error:
-        send_text = error
+def quote_type_handler(message):
+    send_text = "What type of quotes do you want? Choose one of the following\nInspirational\nFitness\nLeadership\nSuccess "
+    sent_msg = bot.send_message(message.chat.id, send_text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, quote_sender)
+    
+def quote_sender(message):
+    quote_type = message.text.lower()
+    if quote_type not in ["inspirational", "fitness", "leadership", "success"]:
+        bot.send_message(message.chat.id, "Invalid option, try again by giving \quote")
     else:
-        send_text = """{0}  
-                    *{1}*""".format(quote, author)
-    bot.send_message(message.chat.id, send_text, parse_mode="Markdown")
+        quote, author, error = quotes_getter.get_my_quote(category=quote_type)
+        if error:
+            send_text = error
+        else:
+            send_text = """{0}  
+                        *{1}*""".format(quote, author)
+        bot.send_message(message.chat.id, send_text, parse_mode="Markdown")
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
-    bot.reply_to(message, message)
+    bot.send_message(message.chat.id, "Invalid option, try again by giving \quote")
 
 bot.infinity_polling()
 
